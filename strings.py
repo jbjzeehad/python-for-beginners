b97866a0026f10273d0c2051ab0ef651292bdadd
jbjzeehad <ilamta.97@gmail.com>
2022-05-01 18:01:47 +0600

    string ing

--- strings.py
+++ strings.py
@@ -2,3 +2,12 @@
 
 print ("Remember ! the double quotation") #both double ""
 print ('Remember ! the single quotation') #both single ''
+
+#everything in quotes is a string
+
+print ("42")
+print('24')
+
+#backslash \ is a special character called escape character
+
+print('Rahim\'s friend Karim is my brother. He\'s a very good person')
