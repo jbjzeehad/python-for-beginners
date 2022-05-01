65bf8291eab0c7a4d1303f6af7f8dc6fb764f5fb
jbjzeehad <ilamta.97@gmail.com>
2022-05-01 15:38:22 +0600

    how to comment

--- basic.py
+++ basic.py
@@ -1,7 +1,10 @@
-print ("Hello world") 
-print ('Jbj Zeehad') 
+#print number and text
+
+print ("Hello world") #both double quotes or
+print ('Jbj Zeehad') #both single quotes
 print ("New language")
 
+#print number without quotes
 
-print (125)
+print (125) 
 print (17.01)
