29346de4d5ff9322abee32512a11321559359887
jbjzeehad <ilamta.97@gmail.com>
2022-05-01 15:56:26 +0600

    operations

--- basic.py
+++ basic.py
@@ -3,8 +3,17 @@
 print ("Hello world") #both double quotes or
 print ('Jbj Zeehad') #both single quotes
 print ("New language")
-
+ 
 #print number without quotes
 
 print (125) 
 print (17.01)
+
+#print simple operations
+
+print (1 + 2 ) #add
+print (5 - 4 + 3) #sub
+print (4 * 5) #mul
+print (10 / 2) #div > always produce decimal or float
+print (4 * (6 - 2)) #can use () like regular math 
+print (-8 * 6) #negative numbers
