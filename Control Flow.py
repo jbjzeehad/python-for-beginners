0bb0db101433ee4127f4e87a295aa78f4e1a44d2
jbjzeehad <ilamta.97@gmail.com>
2022-05-05 00:21:22 +0600

    part 4

---
+++ Control Flow.py
@@ -0,0 +1,66 @@
+# @ Jbj Zeehad
+
+x=True
+print(x) #boolean : True or False
+
+print(2 == 3) #value compare
+
+print("hello" == "hello") #string compare
+
+x=7;
+
+print(x != 8) #true
+print(x > 5)  #true
+print (x < 2) #false
+print( x >=7) #true
+print(x <= 7) #true
+
+print('a' > 'b') #false
+
+y=(7 > 5)
+print(int(y)) #int 0(false) or 1(true)
+
+z=45;
+if z>5 :
+    print("z is greater than 5") #true
+
+age=70
+if age>5 :
+    print("Five") #Five output
+if age>8 :
+    print("Eight") #Eight output
+if age <=47 :
+    print("ok Done") #False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#part 04
