036df01724d4a9a8189758922f51d9f6e3401698
jbjzeehad <ilamta.97@gmail.com>
2022-05-04 17:18:50 +0600

    Part 02

--- strings.py
+++ strings.py
@@ -13,3 +13,32 @@ print('24')
 #backslash \ is a special character called escape character
 
 print('Rahim\'s friend Karim is my brother. He\'s a very good person') #quote in a string using backslash. same as double quotes
+
+print('one\nTwo\nThree') #\n for new line
+
+print("one \t Two \t Three") #\t for tab
+
+print("Hi \r Jbj") #\r for carriage return
+
+print("New line is \\n") #escaping : include the special character
+
+print("""this 
+is  
+multiline
+text""")  #multiline with 3 quotes
+
+# print("this
+# is 
+# text")   not working
+
+print('''I'm learning
+python.
+he's a good 
+boy ''')  #3 quotes no need escaping
+
+print ("Jbj " + 'Zeehad') #concatenation
+print("3" + "2") #32
+print(4 * "Jbj") #4 times
+# print("Jbj" * "Zeehad") don't try this at home
+
+#part 02
