0273175e006525be173bd30cb9d04944c6b525fa
jbjzeehad <ilamta.97@gmail.com>
2022-05-04 20:05:13 +0600

    part 3

---
+++ variables.py
@@ -0,0 +1,50 @@
+# @ Jbj Zeehad
+
+user ="Jbj Zeehad" #user,x,y : variable
+print(user)
+x= 12
+print(x)
+print(x + 4)
+y=3.4
+# 1c=333   can't do this
+print(y)
+
+x=52
+print(x)
+x="many times"
+print(x) #reassigned as many times
+print (x + " you can")
+
+#but try to avoid this :p
+
+del x #del remove a variable. use the deleted variable causes an error
+
+# print(x) #show error
+
+x=8 #but we can reassign
+print(x)
+
+x=input() #input function and returns as a string by default
+print(x)
+
+print("Entered "+x) #always string
+
+name= input("Enter your mail : ") #provide string
+print("Well Done " + name)
+
+age=int (input())
+print(age) #int value
+
+weight=float(input())
+print(weight) #float value
+
+age=42
+print("his age is " + str(age)) #int to string value
+
+x+=2
+print(x) #+,-,*,/,%,**,//
+
+z="Rahim"
+z+="vai"
+print(z) #string concatenation 
+
