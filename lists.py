53f9d0de907f482291fd23400094ddaf46653d2b
jbjzeehad <ilamta.97@gmail.com>
2022-05-05 17:07:57 +0600

    running 5

---
+++ lists.py
@@ -0,0 +1,21 @@
+# @ Jbj Zeehad
+
+words = ["Hello","world","!"] #like array
+print(words[1]) #world
+
+x = ["a", "b", "c"]
+y = [1, 2, 3, 4, 5]    #diff data types
+print(x[1])
+print(y[3])
+
+m=[
+    [1,2,3],
+    [4,5,6]
+]
+print(m[1][2])  #matrix
+
+
+things = ["text",0,[1,2,8],4.56]
+print(things[2][2])  #output: 8
+
+#part 05
