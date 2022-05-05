623c9f818267e406e51d32757d4e779b6091a797
jbjzeehad <ilamta.97@gmail.com>
2022-05-05 16:14:02 +0600

    part 04 done

--- Control Flow.py
+++ Control Flow.py
@@ -62,32 +62,45 @@ else:
   print("Something else") #if else : elif
 
 
+print(1 == 1 and 2== 2)  #and operator : True
+print(1 !=1 and 2 == 2) #false
+
+print( 1 >= 2 or 2 <= 2) #or operator :True
+print (1 != 3 or 2 > 5) #true
+
+print(not 1 == 1) #not operator
+print(not 1 > 7) #True
+
+age = 24
+limit = 18
+height = 191  #compare variable
+if (age > limit and height > 180) :   
+   print("YES")
+
+i=1
+while i<=5:
+  print(i)    #while loop
+  i=i+1
+print("Finished !")  
+
+k = 0
+while True:
+  print(k)
+  k += 1      #break
+  if k >= 5:
+    print("Break")
+    break
+
+print("Finished")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+m = 0
+while m<5:
+  m += 1      #continue
+  if m==3:
+    print("Skipping 3")
+    continue
+  print(i)
 
 
 #part 04
